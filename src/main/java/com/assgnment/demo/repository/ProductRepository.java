b96d8da4679727fc6305e519b189872387e585d1
Prachi Shrivastava <shrivastavaprachi156@gmail.com>
2024-12-13 20:09:49 +0530

    Update ProductRepository.java

--- src/main/java/com/assgnment/demo/repository/ProductRepository.java
+++ src/main/java/com/assgnment/demo/repository/ProductRepository.java
@@ -4,5 +4,5 @@ import com.assgnment.demo.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-    // Define custom queries if needed
+
 }
