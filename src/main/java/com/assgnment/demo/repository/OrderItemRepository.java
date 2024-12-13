8d50e79591c3e730bcd30e4f466b055c1f1dcaa2
Prachi Shrivastava <shrivastavaprachi156@gmail.com>
2024-12-13 20:10:03 +0530

    Update OrderItemRepository.java

--- src/main/java/com/assgnment/demo/repository/OrderItemRepository.java
+++ src/main/java/com/assgnment/demo/repository/OrderItemRepository.java
@@ -4,5 +4,5 @@ import com.assgnment.demo.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
-    // Define custom queries if needed
+ 
 }
