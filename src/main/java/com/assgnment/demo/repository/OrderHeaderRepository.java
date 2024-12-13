f446ee316e54c348b4139426600b90b26f48ffb6
Prachi Shrivastava <shrivastavaprachi156@gmail.com>
2024-12-13 20:08:11 +0530

    Update OrderHeaderRepository.java

--- src/main/java/com/assgnment/demo/repository/OrderHeaderRepository.java
+++ src/main/java/com/assgnment/demo/repository/OrderHeaderRepository.java
@@ -4,5 +4,5 @@ import com.assgnment.demo.entity.OrderHeader;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderHeaderRepository extends JpaRepository<OrderHeader, Long> {
-    // You can add custom query methods if needed
+   
 }
